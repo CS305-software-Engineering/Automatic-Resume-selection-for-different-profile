601bcf63bf5f5e9057c9d35b44b2f7cfa8b1b844
harshsrova <59247407+harshsrova@users.noreply.github.com>
2021-04-22 15:20:07 +0530

    resue

---
+++ resue/lib/Modules/User_details/user_details.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+import 'components/body.dart';
+
+
+class FormScreen extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return FormScreenState();
+  }
+}
+
